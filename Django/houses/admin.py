ed068a9c8f62aca48b3d99a75a51ba0c5b6b7d22
kikiru328 <kikiru328@gmail.com>
2024-05-16 00:34:13 +0900

    Django is insane

--- Django/houses/admin.py
+++ Django/houses/admin.py
@@ -4,4 +4,28 @@ from .models import House
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    pass # 객체 상속 및 복사 (수정하지 않음)
+    #pass # 객체 상속 및 복사 (수정하지 않음)
+    
+    fields = (
+        "name",
+        "address",
+        ("price_per_night", "pets_allowed")
+    )
+    
+    list_display = (
+        "name",
+        "price_per_night",
+        "address",
+        "pets_allowed"
+    )
+    
+    list_filter = (
+        "price_per_night",
+        "pets_allowed"
+    )
+    
+    search_fields = ("address",)
+    
+    list_display_links = ("name", "address")
+    list_editable = ("pets_allowed", ) # list에서 수정 가능
+    #exclude = ("price_per_night",) 
--- Django/houses/models.py
+++ Django/houses/models.py
@@ -5,10 +5,16 @@ from django.db import models
 class House(models.Model):
     
     """Model Definition for Houses"""
+    def __str__(self):
+        return self.name
     
     # describe data
     name = models.CharField(max_length=140) #house 이름
-    price_per_night = models.PositiveIntegerField() #양수
+    price_per_night = models.PositiveIntegerField(
+        verbose_name="Price", 
+        help_text= "Postive Numbers Only") #양수
     description = models.TextField()
     address = models.CharField(max_length=140)
-    pets_allowed = models.BooleanField(default=True)
+    pets_allowed = models.BooleanField(default=True,
+                                       help_text="Does this house allow pets?",
+                                       verbose_name="Pets Allowed?",)

e4616275c281f0eca320f93ff77a78e4d93452ae
kikiru328 <kikiru328@gmail.com>
2024-05-15 22:15:13 +0900

    python basic

---
+++ Django/Note/models.md
@@ -0,0 +1,4 @@
+# APP 만들기
+```bash
+python manage.py startapp houses # houses : Application 이름
+```
--- Python_Basic/Tax Calculator.py
+++ Python_Basic/Tax Calculator.py
@@ -1,5 +1,53 @@
 # 👇🏻 Write your code here 👇🏻:
+def get_yearly_revenue(monthly_revenue: int) -> int:
+    """
+    연간 매출 계산 수식
+    월간매출 * 12개월
+    Args:
+        monthly_revenue (int): 월간매출
+    Returns:
+        int: yearly_revenue, : 연간매출 
+    """
+    return monthly_revenue * 12
 
+def get_yearly_expenses(monthly_expenses: int) -> int:
+    """
+    연간 비용 계산 수식
+    월간비용 * 12개월
+    Args:
+        monthly_expenses (int): 월간비용 
+    Returns:
+        int: yearly_expenses : 연간비용 
+    """
+    return monthly_expenses * 12
+
+def get_tax_amount(profit: int) -> float:
+    """
+    세금 계산 수식
+    $100,000 초과시 세율 25%, 이하 15%
+    Args:
+        profit (int): 이익
+
+    Returns:
+        float: 세금 계산 
+    """
+    if profit > 100000:
+        return profit * 0.25
+    else:
+        return profit * 0.15
+    
+def apply_tax_credits(tax_amount: float, tax_credits: float) -> float:
+    """
+    세금 공제액 수식
+    세금 * 세금 공제율
+    Args:
+        tax_amount (float): 세금
+        tax_credits (float): 세금 공제율
+
+    Returns:
+        float: 세금 공재액
+    """
+    return tax_amount * tax_credits
 # ❌ Don't touch anthing below this line ❌
 
 monthly_revenue = 5500000
